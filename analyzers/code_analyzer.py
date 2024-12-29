dd219f30f61020394679cb2c25d1f16f11b9f98a
ankushgpta2 <ankushgpta2@gmail.com>
2024-12-29 07:17:42 -0500

    class for code analyzer (does simple syntax checks and other common pitfalls without using agent)

--- analyzers/code_analyzer.py
+++ analyzers/code_analyzer.py
@@ -2,3 +2,10 @@ import ast
 from typing import List
 from ..utils.exceptions import ValidationError
 
+class CodeAnalyzer:
+    """Analyzes code structure and potential issues"""
+    
+    def __init__(self):
+        self.issues: List[str] = []
+    
+    
