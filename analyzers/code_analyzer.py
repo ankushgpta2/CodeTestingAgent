e750acf7290966237af4c3ce4b9e009b01162666
ankushgpta2 <ankushgpta2@gmail.com>
2024-12-30 09:26:52 -0500

    functionality added to check basic syntax structure

--- analyzers/code_analyzer.py
+++ analyzers/code_analyzer.py
@@ -8,4 +8,13 @@ class CodeAnalyzer:
     def __init__(self):
         self.issues: List[str] = []
     
-    
+    def check_syntax(self, tree: ast.AST) -> None:
+        """Check basic syntax structures"""
+        for node in ast.walk(tree):
+            if isinstance(node, ast.ExceptHandler) and node.type is None:
+                self.issues.append("Warning: Using bare 'except' clause - consider catching specific exceptions")
+            
+            if isinstance(node, ast.FunctionDef):
+                for default in node.args.defaults:
+                    if isinstance(default, (ast.List, ast.Dict, ast.Set)):
+                        self.issues.append(f"Warning: Mutable default argument in function {node.name}")
