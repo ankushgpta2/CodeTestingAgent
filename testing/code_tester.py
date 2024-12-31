06a9ec561059fdc14ee928fe02280fe5b92439c7
ankushgpta2 <ankushgpta2@gmail.com>
2024-12-31 08:03:35 -0500

    classmethod within CodeTester that is meant to class the entire analyze code process (handles the main testing of the code)

--- testing/code_tester.py
+++ testing/code_tester.py
@@ -16,3 +16,13 @@ class CodeTester:
         self.analyzer = CodeAnalyzer()
         openai.api_key = llm_api_key
     
+    def test_code(self, code_str, spec) -> TestResult:
+        """Test code against given specifications"""
+        issues = self.analyzer.analyze_code(code_str)
+        if issues:
+            return TestResult(
+                passed=False,
+                error_message="Static analysis found potential issues",
+                feedback="\n".join(issues)
+            )
+        
