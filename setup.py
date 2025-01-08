958f873fc185fb874d00e3223b21f08e1a0f1824
ankushgpta2 <ankushgpta2@gmail.com>
2025-01-08 10:32:28 -0500

    added development status and intended audience information

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/code_testing_agent",
     classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.8",
     ],
